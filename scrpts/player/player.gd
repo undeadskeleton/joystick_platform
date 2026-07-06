74bb1ccd4d99bbae689aa7e8c1057f5e57ca3f03
undeadskeleton <jokerlupe1234@gmail.com>
2026-07-06 20:59:57 +0530

    Last Sync: 2026-07-06 20:59 (Mobile)

--- scrpts/player/player.gd
+++ scrpts/player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @export var joystick : VirJoystick
-var direction : Vector2 
+var direction : Vector2
 
 func _physics_process(delta: float) -> void:
 	direction = joystick.posVector
--- scrpts/virtual_joystick/knob.gd
+++ scrpts/virtual_joystick/knob.gd
@@ -15,7 +15,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	parent.button_action = pressing
 	if pressing:
+
 		if parent.global_position.distance_to(get_global_mouse_position()) <= maxLength:
+			print("pressing")
 			global_position = get_global_mouse_position()
 		else:
 			var angle = parent.global_position.angle_to_point(get_global_mouse_position())
@@ -23,6 +25,7 @@ func _process(delta: float) -> void:
 			global_position.y = parent.global_position.y + sin(angle) * maxLength
 		calculateVector()
 	else:
+		print("not pressing")
 		global_position = lerp(global_position,parent.global_position,delta*50)
 		parent.posVector = Vector2(0,0)
 
@@ -33,11 +36,11 @@ func calculateVector()-> void:
 	if abs(global_position.y-parent.global_position.y) >= deadZone:
 		print("y: ",(global_position.y-parent.global_position.y)/maxLength)
 		parent.posVector.y = (global_position.y-parent.global_position.y)/maxLength
-		
+
 	#Map the vector strenght directly to godot input system
 
 func _on_button_button_down() -> void:
-	pressing= true
+	pressing = true
 
 func _on_button_button_up() -> void:
-	pressing =false
+	pressing = false
