06dca20693db2e507d730c02aa346fc27abc09ca
undead skeleton <safepeople08@gmail.com>
2026-07-06 22:22:33 +0530

    Fixing dash not showing in action in touchscreenbutton inspect

--- scrpts/state_machine/state_interface.gd
+++ scrpts/state_machine/state_interface.gd
@@ -4,22 +4,16 @@ class_name StateInterface
 
 var statemachine : StateMachine
 
-@export Dash_Speed : int = 200
-@export var dash_timer : float = 0.6
-var can_dash : bool = false
-var store_dir : float
 func enter()-> void:
-	can_dash= true
-
+	pass
 
 func physics_process(delta : float)-> void:
-	var player = statemachine.player_ref
+	pass
 
-	if can_dash:
-		timer-= delta
-		player.velocity.x =
+func process(delta : float)-> void:
+	pass
 
-func handle_input(event: InputEvent)-> void:
+func handle_input(event : InputEvent)-> void:
 	pass
 
 func exit()-> void:
