266eb3bbd645a2fb7acd579e7a8e64f6e9c50f96
undead skeleton <safepeople08@gmail.com>
2026-07-13 20:16:57 +0530

    solved the state transition problem

--- scrpts/state_machine/fall.gd
+++ scrpts/state_machine/fall.gd
@@ -6,14 +6,10 @@ func physics_process(delta : float)-> void:
 	var player = statemachine.player_ref
 	
 	player.velocity.y += 980 * delta
-	
 	if player.is_on_floor():
 		player.velocity.x = move_toward(player.velocity.x,0,50)
-
-func handle_input(event : InputEvent)-> void:
-	var player = statemachine.player_ref
-	if player.is_on_floor():
+		if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+			statemachine.changeState("walk")
+			return
 		if player.velocity.x == 0:
 			statemachine.changeState("idle")
-		if Input.is_action_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
-			statemachine.changeState("walk")
