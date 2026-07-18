4972344d90fb7a6a05ddd284c27f050b16c81b98
undead skeleton <safepeople08@gmail.com>
2026-07-18 23:22:28 +0530

    added button visible only touchscreen

--- scrpts/virtual_joystick/virtual_joystick.gd
+++ scrpts/virtual_joystick/virtual_joystick.gd
@@ -5,7 +5,10 @@ var button_action : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if DisplayServer.is_touchscreen_available():
+		visible = true
+	else:
+		visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
